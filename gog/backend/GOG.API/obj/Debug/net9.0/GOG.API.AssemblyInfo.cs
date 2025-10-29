615afb7b91c634b4c58b2c930c665622e53fe5de
Siya <107757390+Siya-S10-ai@users.noreply.github.com>
2025-10-29 16:35:46 +0200

    Update project structure and configuration files for GOG.API

--- gog/backend/GOG.API/obj/Debug/net9.0/GOG.API.AssemblyInfo.cs
+++ gog/backend/GOG.API/obj/Debug/net9.0/GOG.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GOG.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71e748999b1eeea970bcc99b8b18cfcb25eb3be3")]
 [assembly: System.Reflection.AssemblyProductAttribute("GOG.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GOG.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
