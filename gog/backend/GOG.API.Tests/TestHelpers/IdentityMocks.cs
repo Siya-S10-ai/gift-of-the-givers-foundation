2cb15f0401a7fabd2c580bf01de9e3b8240111ea
Siya <107757390+Siya-S10-ai@users.noreply.github.com>
2025-11-02 15:54:36 +0200

    Refactor IdentityMocks to improve structure and readability

--- gog/backend/GOG.API.Tests/TestHelpers/IdentityMocks.cs
+++ gog/backend/GOG.API.Tests/TestHelpers/IdentityMocks.cs
@@ -1,41 +1,42 @@
-using Microsoft.AspNetCore.Identity;
-using Moq;
-using System.Collections.Generic;
-using System.Linq;
-using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Moq;
+using System;
 
-public static class IdentityMocks
+namespace GOG.API.Tests.TestHelpers
 {
-    public static Mock<UserManager<TUser>> MockUserManager<TUser>() where TUser : class
+    public static class IdentityMocks
     {
-        var store = new Mock<TUserStore<TUser>>();
-        var mgr = new Mock<UserManager<TUser>>(store.Object,
-            Mock.Of<IOptions<IdentityOptions>>(),
-            Mock.Of<IPasswordHasher<TUser>>(),
-            new IUserValidator<TUser>[0],
-            new IPasswordValidator<TUser>[0],
-            Mock.Of<ILookupNormalizer>(),
-            Mock.Of<IdentityErrorDescriber>(),
-            Mock.Of<IServiceProvider>(),
-            Mock.Of<ILogger<UserManager<TUser>>>());
+        public static Mock<UserManager<TUser>> MockUserManager<TUser>() where TUser : class
+        {
+            var store = new Mock<TUserStore<TUser>>();
+            var mgr = new Mock<UserManager<TUser>>(store.Object,
+                Mock.Of<IOptions<IdentityOptions>>(),
+                Mock.Of<IPasswordHasher<TUser>>(),
+                Array.Empty<IUserValidator<TUser>(),
+                Array.Empty<IPasswordValidator<TUser>(),
+                Mock.Of<ILookupNormalizer>(),
+                Mock.Of<IdentityErrorDescriber>(),
+                Mock.Of<IServiceProvider>(),
+                Mock.Of<ILogger<UserManager<TUser>>>());
 
-        mgr.Object.UserValidators.Clear();
-        mgr.Object.PasswordValidators.Clear();
+            return mgr;
+        }
 
-        return mgr;
-    }
+        public static Mock<SignInManager<TUser>> MockSignInManager<TUser>(Mock<UserManager<TUser>> userManager) where TUser : class
+        {
+            var contextAccessor = new Mock<IHttpContextAccessor>();
+            var claimsFactory = new Mock<TUserClaimPrincipalFactory<TUser>>();
+            var schemes = Mock.Of<Microsoft.AspNetCore.Authentication.IAuthenticationSchemeProvider>();
 
-    public static Mock<SignInManager<TUser>> MockSignInManager<TUser>(Mock<UserManager<TUser>>userManager) where TUser : class
-    {
-        var contextAccessor = new Mock<IHttpContextAccessor>();
-        var claimsFactory = new Mock<IUserClaimPrincipalFactory<TUser>>();
-        return new Mock<SignInManager<IUser>>(userManager.Object,
-            contextAccessor.Object,
-            claimsFactory.Object,
-            Mock.Of<IOptions<IdentityOptions>>(),
-            Mock.Of<ILogger<SignInManager<TUser>>>(),
-            Mock.Of<IAuthenticationSchemaProvider>());
+            return new Mock<SignInManager<TUser>>(userManager.Object,
+                contextAccessor.Object,
+                claimsFactory.Object,
+                Mock.Of<IOptions<IdentityOptions>>(),
+                Mock.Of<ILogger<SignInManager<TUser>>>(),
+                schemes);
+        }
     }
 }
